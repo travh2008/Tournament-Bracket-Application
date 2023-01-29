b83a6fdd06bc77a173a905d05435b3be6761f913
Travis <30766297+travh2008@users.noreply.github.com>
2023-01-29 17:54:09 -0500

    Add files via upload

---
+++ TeamModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public list<Person> TeamMembers { get; set; }
+        public string TeamName { get; set; }
+
+        public TeamModel()
+        {
+            TeamMembers = new list<Person>();
+        }
+    }
+}
---
+++ TournamentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; }
+        public List<PrizeModel> Prizes { get; set; }
+        public List<List<MatchupModel> Rounds MyProperty { get; set; }
+
+        public TournamentModel()
+        {
+            EnteredTeams = new list<TeamModel>();
+            Prizes = new List<PrizeModel>();
+        }
+    }
+}
